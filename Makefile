5b659754c4c5b30fd17d629f1bfde9e964a580f0
Gabriel Mota <gabrielmotaa@hotmail.com>
2021-04-20 15:45:59 -0300

    finished actions, makefile and setup

---
+++ Makefile
@@ -0,0 +1,21 @@
+black:
+	black cypher tests setup.py --check
+
+flake:
+	flake8 cypher tests setup.py
+
+test:
+	pytest
+
+check: black flake test
+
+
+install:
+	python -m pip install -e .
+
+install-dev:
+	python -m pip install -e ".[dev]"
+	pre-commit install
+
+install-test:
+	python -m pip isntall -e ".[test]"
--- setup.py
+++ setup.py
@@ -1,7 +1,22 @@
 from setuptools import setup, find_packages
 
+base_packages = []
+
+test_packages = [
+    "pytest>=6.2.3",
+    "black>=20.8b1",
+    "flake8>=3.9.1",
+]
+
+util_packages = [
+    "pre-commit>=2.12.1",
+]
+
+dev_packages = test_packages + util_packages
+
 setup(
     name="cypher",
     version="0.0.1",
     packages=find_packages(),
+    extra_require={"dev": dev_packages, "test": test_packages},
 )
